735157faef56362ab2ac4bfe6cfecc1bcb66c476
lsfullstack <lsfullstack@gmail.com>
2025-10-30 11:14:23 -0300

    fix

--- src/pages/app/home.tsx
+++ src/pages/app/home.tsx
@@ -4,7 +4,7 @@ export function Home() {
   const links = (() => {
     try {
       const urls = JSON.parse(env.VITE_VIDEO_URLS || '[]')
-      return urls.sort((a, b) => a.order - b.order)
+      return urls.sort((a: any, b: any) => a.order - b.order)
     } catch (error) {
       console.error('Erro ao processar VIDEO_URLS:', error)
       return []
@@ -13,17 +13,22 @@ export function Home() {
 
   return (
     <div className="flex flex-col w-full p-12 bg-slate-900 items-center gap-12">
-      {links.map((link, index) => (
-        <div key={index} className="w-full max-w-[80%] aspect-video">
-          <h2 className="text-2xl font-bold">{link.title}</h2>
-          <iframe
-            className="w-full h-full"
-            src={`https://drive.google.com/file/d/${link.url}/preview`}
-            allow="autoplay"
-            allowFullScreen
-          />
-        </div>
-      ))}
+      {links.map(
+        (
+          link: { title: string; url: string; order: number },
+          index: number,
+        ) => (
+          <div key={index} className="w-full max-w-[80%] aspect-video">
+            <h2 className="text-2xl font-bold">{link.title}</h2>
+            <iframe
+              className="w-full h-full"
+              src={`https://drive.google.com/file/d/${link.url}/preview`}
+              allow="autoplay"
+              allowFullScreen
+            />
+          </div>
+        ),
+      )}
     </div>
   )
 }
