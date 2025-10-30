0970479686ccff35faf62b0c09de707cc4c47089
lsfullstack <lsfullstack@gmail.com>
2025-10-30 11:53:21 -0300

    fix layout

--- src/pages/app/home.tsx
+++ src/pages/app/home.tsx
@@ -12,23 +12,34 @@ export function Home() {
   })()
 
   return (
-    <div className="flex flex-col h-full min-h-screen w-full p-12 bg-slate-900 items-center gap-12">
+    <div className="flex flex-col min-h-screen w-full p-4 lg:p-12 bg-slate-900 items-center gap-10">
       {links.length === 0 && (
-        <p className="text-center text-xl">Nenhum vídeo encontrado.</p>
+        <p className="text-center text-xl text-white">
+          Nenhum vídeo encontrado.
+        </p>
       )}
+
       {links.map(
         (
           link: { title: string; url: string; order: number },
           index: number,
         ) => (
-          <div key={index} className="w-full max-w-[80%] aspect-video">
-            <h2 className="text-2xl font-bold">{link.title}</h2>
-            <iframe
-              className="w-full h-full"
-              src={`https://drive.google.com/file/d/${link.url}/preview`}
-              allow="autoplay"
-              allowFullScreen
-            />
+          <div
+            key={index}
+            className="flex flex-col w-full lg:max-w-[80%] gap-3 bg-slate-800 rounded-2xl p-4 shadow-md"
+          >
+            <h2 className="text-lg lg:text-2xl font-bold text-white text-center">
+              {link.title}
+            </h2>
+
+            <div className="relative w-full aspect-video rounded-xl overflow-hidden">
+              <iframe
+                className="absolute top-0 left-0 w-full h-full"
+                src={`https://drive.google.com/file/d/${link.url}/preview`}
+                allow="autoplay"
+                allowFullScreen
+              />
+            </div>
           </div>
         ),
       )}
